1657fe7f971d4174ab7008fda0b5685b36177771
mbsyaswanth <mbs.yaswanth@gmail.com>
2019-06-28 22:28:48 +0530

    done problem 8 and 9

--- Assignment-4/problem8.js
+++ Assignment-4/problem8.js
@@ -1,11 +1,22 @@
 let longestWord = (str) => {
     var words=str.split(" ");
-    for (i=0;i<words.lenth;i++){
-        for(j=0;j<words.lenght;j++){
-            if(words[i].length<words[i+1]){
-                break;
+    for(i=0;i<words.length;i++){
+        for(j=0;j<words.length;j++){
+            if(words[i].length<=words[j].length){
+                if(j==(words.length-1)){
+                    console.log("in if",words[i]);
+                    return words[i];
+                } else if(i!=j) {
+                    //console.log(words[i]);
+                    break;
+                }
+            } else{
+                return words[i];
             }
         }  
     }
     
 }
+
+let strng='You progress the nation follows';
+console.log(longestWord(strng));
---
+++ Assignment-4/problem9.js
@@ -0,0 +1,15 @@
+let leftRightofLetter= (str,letter) => {
+    let index=str.indexOf(letter);
+    if(index!=-1){
+        if(index==0){
+            console.log(str[1]);
+        }
+        else if(index==str.length-1){
+            console.log(str[length-2]);
+        } else {
+            console.log(str[index-1]+""+str[index+1]);
+        }
+    }
+}
+
+leftRightofLetter("IBHUBS","U");
