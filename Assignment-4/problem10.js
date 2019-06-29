9b5e880b764f344124494192fb9f78e48e202703
ib_admin <mbs.yaswanth@gmail.com>
2019-06-29 16:51:58 +0530

    sucessfully completed assignment 4. problem 10 and 11 done

--- Assignment-4/problem10.js
+++ Assignment-4/problem10.js
@@ -1,35 +1,40 @@
 let duplicateRemover = (arrofobj) => {
-    let ids=[];
- let filobj=arrofobj.maps(
-     (obj) => {
-         if(id.includes(obj.id)){
-             arrofobj.filter(obj=>!id.includes(obj.id));
-         }
-         ids.concat(obj.id);
-     }
- )
-    gradeStats(filobj);
+    let ids=[]; let filobj=arrofobj;
+
+     let filtered = arrofobj.filter(function(value){
+        if(!ids.includes(value.id)){
+            ids.push(value.id);
+            return value;
+         }  
+    
+    });
+//  console.log()
+//  console.log(filtered);
+    return gradeStats(filtered);
 }
 
 let gradeStats = (objs) => {
     let agrade=0;let bgrade=0; let cgrade=0; let fail=0;let distinction=0;
    objs.map((obj)=>{
        grade=obj.gpa;
-    switch (grade) {
-        case grade>=8:
-            distinction++;
-            break;
-        case grade>=7 && grade<7:
-            agrade++;
-            break;
-        case grade>=6 && grade<7:
-            bgrade++;
-            break;
-        case grade>=4 && grade<6:
-            cgrade++;
-        case grade<4:
-            fail++;
-    }
+       if(grade>=8){
+        distinction++;
+       } else if(grade>=7 && grade<8){
+        agrade++;
+       } else if(grade>=6 && grade<7)
+       {
+        bgrade++;
+       } else if(grade>=4 && grade<6) 
+       {
+        cgrade++;
+       } else
+       {
+        fail++;
+       }
    });
-   return {distinction,agrade,bgrade,cgrade,fail}
+   return {distinction,agrade,bgrade,cgrade,fail};
 }
+
+let arobj=[ { "name" : "John", "id": 1, "gpa" : 7.8 }, { "name" : "Rick", "id": 2, "gpa" : 3.9 }, { "name" : "Lisa", "id": 3, "gpa" : 8.9 }, { "name" : "John", "id": 1, "gpa" : 8.9 } ];
+
+    console.log(duplicateRemover(arobj));
---
+++ Assignment-4/problem11.js
@@ -0,0 +1,141 @@
+let carslist = [
+    {
+        id: '5',
+        color: 'black',
+        speed: '120',
+        model: 'maruthi',
+        fuel: 'petrol'
+    },
+    {
+        id: '2',
+        color: 'white',
+        speed: '150',
+        model: 'tesla',
+        fuel: 'battery'
+    },
+    {
+        id: '3',
+        color: 'red',
+        speed: '130',
+        model: 'ford',
+        fuel: 'petrol'
+    }
+];
+
+function showroom(id, cars, addr) {
+    this.id = id;
+    this.cars = cars;
+    this.addr = addr;
+    this.addcar = function (car) {
+        if(!this.checkCarAvailability(car)){
+            this.cars.push(car);
+            console.log("car added to showroom :",this.cars);
+        } else {
+            console.log("car already available in showroom ",this.cars);
+        }
+    };
+    this.checkCarAvailability = function (car) {
+        for (i = 0; i < this.cars.length; i++) {
+            if (this.cars[i].id == car.id) {
+
+                console.log("car found in showroom");
+                return true;
+            }
+        }
+        console.log("car not available in showroom. please choose another car");
+        return false;
+    };
+    this.removecar = function (rcar) {
+        if (this.checkCarAvailability(rcar)) {
+            let filcar = this.cars.filter(function (car) {
+                return car.id !== rcar.id;
+            });
+            if (filcar.length == this.cars.length) {
+                console.log("failed to remove car.");
+                return false;
+            } else {
+                this.cars = filcar;
+                console.log("removed the car from showroom :" + this.cars)
+                return true;
+            }
+        } else{
+            console.log("car not available in showroom");
+        }
+    };
+}
+
+let showroom1 = new showroom(1, carslist, "Madhapur");
+
+let PersonA = {
+    firstName: "b",
+    lastName: "kiran",
+    carsWishList: [],
+    carsOwned: [],
+    buyCar: function(car, showroom){
+        if(!this.searchOwnedCars(car)){
+            if (showroom.checkCarAvailability(car)) {
+                if(this.searchWishlist(car)){
+                    this.removeFromWishlist(car);
+                }
+                if(showroom.removecar(car)){
+                    this.carsOwned.push(car);
+                    console.log("car added to person's car owned list : " + this.carsOwned);
+                }
+            };
+        }
+    },
+    searchOwnedCars: function(car) {
+        for (i = 0; i < this.carsOwned.length; i++) {
+            if (this.carsOwned[i].id == car.id) {
+                console.log("You already own this car");
+                return true;
+            }
+        }
+        console.log("You dont have this car.");
+        return false;
+    },
+    addToWishlist: function (car) {
+        if(!this.searchWishlist(car)){
+            this.carsWishList.push(car);
+            console.log("car added to persons wishlist " + " :" + this.carsWishList);
+        }
+    },
+    searchWishlist: function(car) {
+        for (i = 0; i < this.carsWishList.length; i++) {
+            if (this.carsWishList[i].id == car.id) {
+                console.log("car found in wishlist");
+                return true;
+            }
+        }
+        console.log("car not found in wishlist");
+        return false;
+    },
+    removeFromWishlist: function(car) {
+        if (this.searchWishlist(car)) {
+            let filcar = this.carsWishList.filter(function (wcar) {
+                return wcar.id !== car.id;
+            });
+            if (filcar.length == this.carsWishList.length) {
+                console.log("failed to remove car from wishlist.");
+                return false;
+            } else {
+                this.carsWishList = filcar;
+                console.log("removed the car from wishlist :", this.cars)
+                return true;
+            }
+        }
+    }
+}
+
+// test 
+
+console.log("all available cars in showroom :" + showroom1.cars);
+PersonA.buyCar({
+    id: '2',
+    color: 'white',
+    speed: '150',
+    model: 'tesla',
+    fuel: 'battery'
+}, showroom1);
+
+
